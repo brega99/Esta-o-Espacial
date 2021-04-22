17138518c97bdb7fa287ab9c2c240549a9217e55
brega99 <54276781+brega99@users.noreply.github.com>
2021-04-22 13:53:32 -0300

    Initial commit

---
+++ Assets/Scripts/AgenteManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgenteManager : MonoBehaviour
+{ 
+    GameObject[] agents;//Menciona os Agentes em cena
+   void Start () 
+   {
+        agents = GameObject.FindGameObjectsWithTag("AI");//Aciona objetos em cena com a Tag AI
+   }
+    
+   void Update () 
+   {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200))
+                {
+                    foreach(GameObject a in agents)
+                    a.GetComponent<AiControl>().agent.SetDestination(hit.point);
+            
+                }
+        }
+    }
+}
---
+++ Assets/Scripts/AiControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+        
+      void  Start ()
+      {
+        agent= this.GetComponent<NavMeshAgent>(); //Determina que os agents sigam a rota do navmesh
+      }
+}
